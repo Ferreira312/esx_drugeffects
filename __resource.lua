cbf9d837715be6a761d5e02fcd8b0aa58bd4a982
MrLumi <lumilesff@hotmail.com>
2018-02-16 15:14:21 +0000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,13 +1,11 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Optional Needs'
+description 'ESX Drug Effects'
 
-version '1.0.0'
+version '0.0.5'
 
 server_scripts {
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
 	'config.lua',
 	'server/main.lua'
 }
