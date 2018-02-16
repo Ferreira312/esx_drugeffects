f8ce0118f1d1ceb13e3dce8d49f0d943d766a0de
MrLumi <lumilesff@hotmail.com>
2018-02-16 16:07:07 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -9,7 +9,7 @@ ESX.RegisterUsableItem('weed', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.removeInventoryItem('weed', 1)
 
-	TriggerClientEvent('esx_status:add', source, 'drug', 5000)
+	TriggerClientEvent('esx_status:add', source, 'drug', 150000)
 	TriggerClientEvent('esx_drugseffects:onDrug', source)
 end)
 
@@ -18,7 +18,7 @@ ESX.RegisterUsableItem('opium', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.removeInventoryItem('opium', 1)
 
-	TriggerClientEvent('esx_status:add', source, 'drug', 7000)
+	TriggerClientEvent('esx_status:add', source, 'drug', 170000)
 	TriggerClientEvent('esx_drugseffects:onDrug', source)
 end)
 
@@ -27,7 +27,7 @@ ESX.RegisterUsableItem('meth', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.removeInventoryItem('meth', 1)
 
-	TriggerClientEvent('esx_status:add', source, 'drug', 12000)
+	TriggerClientEvent('esx_status:add', source, 'drug', 320000)
 	TriggerClientEvent('esx_drugseffects:onDrug', source)
 end)
 
@@ -36,6 +36,6 @@ ESX.RegisterUsableItem('coke', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.removeInventoryItem('coke', 1)
 
-	TriggerClientEvent('esx_status:add', source, 'drug', 140000)
+	TriggerClientEvent('esx_status:add', source, 'drug', 450000)
 	TriggerClientEvent('esx_drugseffects:onDrug', source)
 end)
