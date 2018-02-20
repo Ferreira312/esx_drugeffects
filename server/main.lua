f980f5f028f71496bcb1affa86d72850a6d3c163
MrLumi <lumilesff@hotmail.com>
2018-02-20 14:43:10 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,41 +1,46 @@
 ESX = nil
 
+
 TriggerEvent('esx:getSharedObject', function(obj)
 	ESX = obj
 end)
 
 ESX.RegisterUsableItem('weed', function(source)
-
-	local xPlayer = ESX.GetPlayerFromId(source)
+        
+        local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
 	xPlayer.removeInventoryItem('weed', 1)
 
-	TriggerClientEvent('esx_status:add', source, 'drug', 12000)
-	TriggerClientEvent('esx_drugseffects:onDrug', source)
+	TriggerClientEvent('esx_status:add', _source, 'drug', 12000)
+	TriggerClientEvent('esx_drugeffects:onWeed', source)
 end)
 
 ESX.RegisterUsableItem('opium', function(source)
-
+       
+        local _source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.removeInventoryItem('opium', 1)
 
-	TriggerClientEvent('esx_status:add', source, 'drug', 37000)
-	TriggerClientEvent('esx_drugseffects:onDrug', source)
+	TriggerClientEvent('esx_status:add', _source, 'drug', 37000)
+	TriggerClientEvent('esx_drugeffects:onOpium', source)
 end)
 
 ESX.RegisterUsableItem('meth', function(source)
-
+        
+        local _source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.removeInventoryItem('meth', 1)
 
-	TriggerClientEvent('esx_status:add', source, 'drug', 110000)
-	TriggerClientEvent('esx_drugseffects:onDrug', source)
+	TriggerClientEvent('esx_status:add', _source, 'drug', 110000)
+	TriggerClientEvent('esx_drugeffects:onMeth', source)
 end)
 
 ESX.RegisterUsableItem('coke', function(source)
-
+        
+        local _source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.removeInventoryItem('coke', 1)
 
-	TriggerClientEvent('esx_status:add', source, 'drug', 333000)
-	TriggerClientEvent('esx_drugseffects:onDrug', source)
+	TriggerClientEvent('esx_status:add', _source, 'drug', 333000)
+	TriggerClientEvent('esx_drugeffects:onCoke', source)
 end)
